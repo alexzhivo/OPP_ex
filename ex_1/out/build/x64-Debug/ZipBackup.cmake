9cfe60e838a8f87cff99c23d8f355c7b58a33899
Alex Zhivot <alexzhivot@gmail.com>
2024-01-01 23:49:00 +0200

    add .gitignore

--- ex_1/out/build/x64-Debug/ZipBackup.cmake
+++
@@ -1,5 +0,0 @@
-string (TIMESTAMP MY_TIMESTAMP "%Y-%m-%d-%H-%M-%S")
-execute_process (COMMAND C:/Program Files/Microsoft Visual Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/bin/cmake.exe --build "C:/Users/Alex/Desktop/תכנות מונחה עצמים/Exercises/OPP_ex/ex_1/out/build/x64-Debug" --target package_source)
-execute_process (COMMAND C:/Program Files/Microsoft Visual Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/bin/cmake.exe -E copy "C:/Users/Alex/Desktop/תכנות מונחה עצמים/Exercises/OPP_ex/ex_1/out/build/x64-Debug/oop1_ex01-Alex_Zhivotovsky-Thai_Ben_Hamo.zip" "C:/Users/Alex/Desktop/תכנות מונחה עצמים/Exercises/OPP_ex/oop1_ex01-Alex_Zhivotovsky-Thai_Ben_Hamo-${MY_TIMESTAMP}.zip")
-execute_process (COMMAND C:/Program Files/Microsoft Visual Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/bin/cmake.exe -E echo "")
-execute_process (COMMAND C:/Program Files/Microsoft Visual Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/bin/cmake.exe -E echo "***" C:/Users/Alex/Desktop/תכנות מונחה עצמים/Exercises/OPP_ex/oop1_ex01-Alex_Zhivotovsky-Thai_Ben_Hamo-${MY_TIMESTAMP}.zip "file created ***")
