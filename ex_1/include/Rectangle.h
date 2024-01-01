8bbde5d94cdca9d6d7c27dc1ac41206d9c25b653
Alex Zhivot <alexzhivot@gmail.com>
2024-01-02 00:34:11 +0200

    fill Rectangle.h file

--- ex_1/include/Rectangle.h
+++ ex_1/include/Rectangle.h
@@ -7,8 +7,22 @@
 class Rectangle {
 
 public:
-	Rectangle();                        // constructor
+	// constructors
+	Rectangle(const Vertex& bottomLeft, const Vertex& topRight);
+	Rectangle(const Vertex vertices[2]);
+	Rectangle(double x0, double y0, double x1, double y1);
+	Rectangle(const Vertex& start, double width, double height);
+	
+	// functions
+	Vertex getBottomLeft() const;
+	Vertex getTopRight() const;
+	double getWidth() const;
+	double getHeight() const;
 
 private:
+	Vertex bottomLeft;
+	Vertex TopRight;
+	double Width;
+	double Height;
 
 }; // end class Rectangle
